c330231c0691630b934f38590a4bac79248cfa9f
xintok <jacintocalderonfernandez@gmail.com>
2020-01-30 20:35:59 +0100

    modelos y controladores

---
+++ database/factories/DiaFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Dia;
+use Faker\Generator as Faker;
+
+$factory->define(Dia::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
