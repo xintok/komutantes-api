8ff813488247faa80a2b68181bc81185ad686208
jacinto <jacintocalderonfernandez@gmail.com>
2020-02-18 13:54:51 +0100

    arreglo https

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     <title>Komutantes</title>
     
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="{{ url('/css/komutantes.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('/css/komutantes.css') }}" />
     <!--     Fonts     -->
     <link href="https://fonts.googleapis.com/css?family=Fredoka+One&display=swap" rel="stylesheet">   
    	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" rel="stylesheet"> 	
